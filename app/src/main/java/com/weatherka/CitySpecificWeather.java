77b8c652408694534d5b798082d5a563a46accc3
MikosJ <jakubdmikos@gmail.com>
2022-12-02 12:57:32 +0100

    sharing city name between two activities

--- app/src/main/java/com/weatherka/CitySpecificWeather.java
+++ app/src/main/java/com/weatherka/CitySpecificWeather.java
@@ -1,5 +1,22 @@
 package com.weatherka;
 
-public class CitySpecificWeather {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CitySpecificWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        Intent city = getIntent();
+        String cityName = city.getStringExtra(Intent.EXTRA_TEXT);
+        TextView cityInfo = (TextView) findViewById(R.id.cityName);
+        cityInfo.setText(cityName);
+    }
 
 }
--- app/src/main/java/com/weatherka/MainActivity.java
+++ app/src/main/java/com/weatherka/MainActivity.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent city = new Intent(MainActivity.this,CitySpecificWeather.class);
+        Button show = (Button) findViewById(R.id.showBtn);
+        show.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CitySpecificWeather.class));
+            }
+        });
     }
+
+
+
+
+    public void onCityInput(View view) {
+        EditText cityInfo = (EditText) findViewById(R.id.cityNameInput);
+        Intent city = new Intent(MainActivity.this, CitySpecificWeather.class);
+        city.putExtra("CITY_NAME", cityInfo.getText().toString());
+        startActivity(city);
+    }
+
 }
