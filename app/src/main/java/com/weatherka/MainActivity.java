3f3f854f0e4454a79b9726c1305a486839290886
MikosJ <jakubdmikos@gmail.com>
2022-12-02 15:44:13 +0100

    to implement: temperature for actual location

--- app/src/main/java/com/weatherka/MainActivity.java
+++ app/src/main/java/com/weatherka/MainActivity.java
@@ -3,10 +3,12 @@ package com.weatherka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.GpsStatus;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button show = (Button) findViewById(R.id.showBtn);
+
+        //Main activity temperature view for actual location, **to implement**
+        //TextView tempAct = (TextView) findViewById(R.id.tempAct);
+
         show.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onCityInput(v);
             }
         });
     }
-
-
-
 
     public void onCityInput(View view) {
         EditText cityInfo = (EditText) findViewById(R.id.cityNameInput);
