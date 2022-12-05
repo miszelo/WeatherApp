fa2c3279599832c950191cbb787a29c0be2bee85
michal <michal.kawczak@gmail.com>
2022-12-05 13:57:09 +0100

    location

--- app/src/main/java/com/weatherka/MainActivity.java
+++ app/src/main/java/com/weatherka/MainActivity.java
@@ -1,89 +1,68 @@
 package com.weatherka;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-
+    private TextView latituteField;
+    private TextView longitudeField;
     private TextView addressText;
-    private Button show;
-    private Button locationButton;
-    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Button showButton;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        show = findViewById(R.id.showBtn);
+        showButton = findViewById(R.id.showBtn);
 
         //Main activity temperature view for actual location, **to implement**
         //TextView tempAct = (TextView) findViewById(R.id.tempAct);
 
-        show.setOnClickListener(this::onCityInput);
+        showButton.setOnClickListener(this::onCityInput);
+        addressText = (TextView) findViewById(R.id.addressText);
+        latituteField = (TextView) findViewById(R.id.latituteField);
+        longitudeField = (TextView) findViewById(R.id.longitudeField);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-
-        locationButton = findViewById(R.id.locationButton);
-        addressText = findViewById(R.id.addressText);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        locationButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
-                } else {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-                }
-            }
-        });
-    }
-
-    private void getLocation() {
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if (location != null) {
-                    try {
-                        Geocoder geocoder = new Geocoder(MainActivity.this,
-                                Locale.getDefault());
-                        List<Address> addressList = geocoder.getFromLocation(
-                                location.getLatitude(), location.getLongitude(), 1
-                        );
-                        addressText.setText(Html.fromHtml(addressList.get(0).getCountryName()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
+        Location location = locationManager.getLastKnownLocation(provider);
+        System.out.println(location);
+
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            latituteField.setText("Location not available");
+            longitudeField.setText("Location not available");
+        }
     }
 
     public void onCityInput(View view) {
@@ -93,4 +72,69 @@ public class MainActivity extends AppCompatActivity {
         startActivity(city);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //You had this as int. It is advised to have Lat/Loing as double.
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        System.out.println(lat);
+        System.out.println(lng);
+
+        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<Address> address = geoCoder.getFromLocation(lat, lng, 1);
+            int maxLines = address.get(0).getMaxAddressLineIndex();
+            for (int i = 0; i < maxLines; i++) {
+                String addressStr = address.get(0).getAddressLine(i);
+                builder.append(addressStr);
+                builder.append(" ");
+            }
+
+            String finalAddress = builder.toString(); //This is the complete address.
+
+            latituteField.setText(String.valueOf(lat));
+            longitudeField.setText(String.valueOf(lng));
+            System.out.println(finalAddress);
+            addressText.setText(finalAddress); //This will display the final address.
+
+        } catch (IOException e) {
+            // Handle IOException
+        } catch (NullPointerException e) {
+            // Handle NullPointerException
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
 }
