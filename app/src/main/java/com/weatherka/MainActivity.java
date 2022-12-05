68b504a532d680054c941f641b013c7ab06d84a8
michal <michal.kawczak@gmail.com>
2022-12-05 21:06:49 +0100

    location work

--- app/src/main/java/com/weatherka/MainActivity.java
+++ app/src/main/java/com/weatherka/MainActivity.java
@@ -5,10 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -17,124 +15,112 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.location.LocationListenerCompat;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListenerCompat {
 
-    private TextView latituteField;
-    private TextView longitudeField;
     private TextView addressText;
     private Button showButton;
     private LocationManager locationManager;
-    private String provider;
+    private EditText cityInfo;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.RequestPermission(), isGranted -> {
+                        if (isGranted) {
+                            Toast.makeText(getApplicationContext(),
+                                    R.string.permissionGranted, Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    R.string.noPermission, Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showButton = findViewById(R.id.showBtn);
 
         //Main activity temperature view for actual location, **to implement**
         //TextView tempAct = (TextView) findViewById(R.id.tempAct);
 
+        showButton = findViewById(R.id.showBtn);
+        cityInfo = findViewById(R.id.cityNameInput);
         showButton.setOnClickListener(this::onCityInput);
-        addressText = (TextView) findViewById(R.id.addressText);
-        latituteField = (TextView) findViewById(R.id.latituteField);
-        longitudeField = (TextView) findViewById(R.id.longitudeField);
+
+        addressText = findViewById(R.id.addressText);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
-        System.out.println(location);
-
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-        } else {
-            latituteField.setText("Location not available");
-            longitudeField.setText("Location not available");
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch((Manifest.permission.ACCESS_FINE_LOCATION));
         }
     }
 
     public void onCityInput(View view) {
-        EditText cityInfo = (EditText) findViewById(R.id.cityNameInput);
+
         Intent city = new Intent(MainActivity.this, CitySpecificWeather.class);
         city.putExtra("CITY_NAME", cityInfo.getText().toString());
         startActivity(city);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+    public void onLocationChanged(@NonNull Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            addressText.setText(addresses.get(0).getLocality());
+            cityInfo.setText(addresses.get(0).getLocality());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, R.string.noPermission, Toast.LENGTH_LONG).show();
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500L, 20.f, this);
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
         locationManager.removeUpdates(this);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        //You had this as int. It is advised to have Lat/Loing as double.
-        double lat = location.getLatitude();
-        double lng = location.getLongitude();
-        System.out.println(lat);
-        System.out.println(lng);
-
-        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
-        StringBuilder builder = new StringBuilder();
-        try {
-            List<Address> address = geoCoder.getFromLocation(lat, lng, 1);
-            int maxLines = address.get(0).getMaxAddressLineIndex();
-            for (int i = 0; i < maxLines; i++) {
-                String addressStr = address.get(0).getAddressLine(i);
-                builder.append(addressStr);
-                builder.append(" ");
-            }
-
-            String finalAddress = builder.toString(); //This is the complete address.
-
-            latituteField.setText(String.valueOf(lat));
-            longitudeField.setText(String.valueOf(lng));
-            System.out.println(finalAddress);
-            addressText.setText(finalAddress); //This will display the final address.
-
-        } catch (IOException e) {
-            // Handle IOException
-        } catch (NullPointerException e) {
-            // Handle NullPointerException
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Toast.makeText(this, "Enabled new provider " + provider,
-                Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(this, "Disabled provider " + provider,
-                Toast.LENGTH_SHORT).show();
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
